cd7cdd9dcf18a5cd23851da455e9b22ae2138262
cez-timescale <cez@timescale.com>
2024-07-18 22:10:03 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,9 +81,9 @@ with compression_tab:
     icon="✍️",
     )
 
-    query = "SELECT pg_size_pretty(before_compression_total_bytes) as before, pg_size_pretty(after_compression_total_bytes) as after FROM hypertable_compression_stats('rides');"
+    query = "SELECT pg_size_pretty(before_compression_total_bytes) as Total_Bytes_Before_Compression, pg_size_pretty(after_compression_total_bytes) as Total_Bytes_After_Compression, round(before_compression_total_bytes / after_compression_total_bytes::numeric, 2) as Compression_Ratio FROM hypertable_compression_stats('rides');"
 
-#   df_compression = conn.query('SELECT pg_size_pretty(before_compression_total_bytes) as before, pg_size_pretty(after_compression_total_bytes) as after FROM hypertable_compression_stats('rides');', ttl="0")   
+#   df_compression = conn.query('SELECT pg_size_pretty(before_compression_total_bytes) as Total_Before_Compression, pg_size_pretty(after_compression_total_bytes) as Total_After_Compression FROM hypertable_compression_stats('rides');', ttl="0")   
     df_compression = conn.query(query, ttl="0")
     st.dataframe(df_compression.set_index(df_compression.columns[0]))
 
