3d04e2a2393c0154262aff23bd4b3617141cfea5
cez-timescale <cez@timescale.com>
2024-07-18 04:32:53 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,25 @@
 import streamlit as st
+import time
 
 st.title("TimescaleDB vs PostgreSQL Perf")
-st.write(
-    "For help head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-st.write(
-    "Querying 10M rides table from NYC can dataset"
-)
-
-st.write(
-    "SQL: SELECT rate_code, COUNT(vendor_id) AS num_trips FROM rides GROUP BY rate_code ORDER BY rate_code;"
-)
-
-
-# streamlit_app.py
-
-import streamlit as st
+st.write("For help head over to [docs.streamlit.io](https://docs.streamlit.io/).")
+st.write("Querying 10M rides table from NYC can dataset")
+st.write("SQL: SELECT rate_code, COUNT(vendor_id) AS num_trips FROM rides GROUP BY rate_code ORDER BY rate_code;")
 
 # Initialize connection.
 conn = st.connection("postgresql", type="sql")
 
-# Perform query.
+# Perform hypertable query.
+start_time = time.time()
 df = conn.query('SELECT rate_code, COUNT(vendor_id) AS num_trips FROM rides GROUP BY rate_code ORDER BY rate_code;', ttl="0")
+end_time = time.time()
 
 # Print results.
 st.write("Hypertable Query")
 st.dataframe(df, use_container_width=True)
+print ("Hyoertable Query Complete - Elapsed {0:4.1f}s".format ((end_time - start_time)))  
 
-# Perform query.
+# Perform pg query.
 df_pg = conn.query('SELECT rate_code, COUNT(vendor_id) AS num_trips FROM rides_pg_table GROUP BY rate_code ORDER BY rate_code;', ttl="0")
 
 # Print results.
