a28b3fff5989663e14fe0c86a84f703da370a027
cez-timescale <cez@timescale.com>
2024-07-18 18:43:24 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,46 @@
 import streamlit as st
 import time
 
+st.title("TimescaleDB vs PostgreSQL")
+st.write("Try Timescale for Free [Link](https://www.timescale.com/?utm_source=google&utm_medium=cpc&utm_campaign=brand&utm_term=timescale&utm_campaign=g_search_brand&utm_source=adwords&utm_medium=ppc&hsa_acc=9771591554&hsa_cam=20915411738&hsa_grp=163076503891&hsa_ad=686577754033&hsa_src=g&hsa_tgt=kwd-11120288971&hsa_kw=timescale&hsa_mt=p&hsa_net=adwords&hsa_ver=3&gad_source=1&gclid=Cj0KCQjw-uK0BhC0ARIsANQtgGNVDQ2KKT5_xr-baYDL-aLL-qiLpHnEl23YqAjm-dM3oL3AEB8DMj8aAvIbEALw_wcB).")
+
 # Set up tabs
 performace_tab, compression_tab, continuous_aggregation_tab = st.tabs(["Performance", "Compression", "Continuous Aggregation"])
 
 with performace_tab:
+    st.write("Querying 10M rides table from NYC cab dataset")
+    st.write("SQL Executed: SELECT rate_code, COUNT(vendor_id) AS num_trips FROM rides GROUP BY rate_code ORDER BY rate_code;")
 
-st.title("TimescaleDB vs PostgreSQL")
-st.write("Try Timescale for Free [Link](https://www.timescale.com/?utm_source=google&utm_medium=cpc&utm_campaign=brand&utm_term=timescale&utm_campaign=g_search_brand&utm_source=adwords&utm_medium=ppc&hsa_acc=9771591554&hsa_cam=20915411738&hsa_grp=163076503891&hsa_ad=686577754033&hsa_src=g&hsa_tgt=kwd-11120288971&hsa_kw=timescale&hsa_mt=p&hsa_net=adwords&hsa_ver=3&gad_source=1&gclid=Cj0KCQjw-uK0BhC0ARIsANQtgGNVDQ2KKT5_xr-baYDL-aLL-qiLpHnEl23YqAjm-dM3oL3AEB8DMj8aAvIbEALw_wcB).")
-st.write("Querying 10M rides table from NYC cab dataset")
-st.write("SQL Executed: SELECT rate_code, COUNT(vendor_id) AS num_trips FROM rides GROUP BY rate_code ORDER BY rate_code;")
+    # Initialize connection.
+    conn = st.connection("postgresql", type="sql")
 
-# Initialize connection.
-conn = st.connection("postgresql", type="sql")
+    # Perform hypertable query.
+    hypertable_start_time = time.time()
+    df = conn.query('SELECT rate_code, COUNT(vendor_id) AS num_trips FROM rides GROUP BY rate_code ORDER BY rate_code;', ttl="0")
+    hypertable_end_time = time.time()
 
-# Perform hypertable query.
-hypertable_start_time = time.time()
-df = conn.query('SELECT rate_code, COUNT(vendor_id) AS num_trips FROM rides GROUP BY rate_code ORDER BY rate_code;', ttl="0")
-hypertable_end_time = time.time()
+    # Perform pg query.
+    postgresql_start_time = time.time()
+    df_pg = conn.query('SELECT rate_code, COUNT(vendor_id) AS num_trips FROM rides_pg_table GROUP BY rate_code ORDER BY rate_code;', ttl="0")
+    postgresql_end_time = time.time()
 
-# Perform pg query.
-postgresql_start_time = time.time()
-df_pg = conn.query('SELECT rate_code, COUNT(vendor_id) AS num_trips FROM rides_pg_table GROUP BY rate_code ORDER BY rate_code;', ttl="0")
-postgresql_end_time = time.time()
+    # Print results.
+    data_container = st.container()
 
-# Print results.
-data_container = st.container()
+    with data_container:
 
-with data_container:
+        postgresql, timescale  = st.columns(2)
 
-    postgresql, timescale  = st.columns(2)
+        with timescale:
+            st.subheader("Hypertable - {0:4.1f} sec".format ((hypertable_end_time - hypertable_start_time)))  
+            st.dataframe(df.set_index(df.columns[0]))
+            st.write(f"**Convert standard table into hypertable partitioned on the time column:**")
+            st.write("SELECT create_hypertable('rides', by_range('pickup_datetime'), create_default_indexes=>FALSE);")
+            st.write(f"**Additional partitioning dimension to speed up queries:**")
+            st.write("SELECT add_dimension('rides', by_hash('payment_type', 2));")
 
-    with timescale:
-        st.subheader("Hypertable - {0:4.1f} sec".format ((hypertable_end_time - hypertable_start_time)))  
-        st.dataframe(df.set_index(df.columns[0]))
-        st.write(f"**Convert standard table into hypertable partitioned on the time column:**")
-        st.write("SELECT create_hypertable('rides', by_range('pickup_datetime'), create_default_indexes=>FALSE);")
-        st.write(f"**Additional partitioning dimension to speed up queries:**")
-        st.write("SELECT add_dimension('rides', by_hash('payment_type', 2));")
-
-    with postgresql:
-        st.subheader("PostgreSQL - {0:4.1f} sec".format ((postgresql_end_time - postgresql_start_time))) 
-        st.dataframe(df_pg.set_index(df_pg.columns[0]))
-        st.write(f"**Create a pg table:**")
-        st.write("CREATE TABLE  rides (vendor_id TEXT, pickup_datetime TIMESTAMP WITHOUT TIME ZONE NOT NULL, dropoff_datetime TIMESTAMP WITHOUT TIME ZONE NOT NULL, passenger_count NUMERIC, trip_distance NUMERIC, pickup_longitude NUMERIC, pickup_latitude NUMERIC, rate_code INTEGER, dropoff_longitude NUMERIC, dropoff_latitude  NUMERIC, payment_type INTEGER, fare_amount NUMERIC, extra NUMERIC, mta_tax NUMERIC, tip_amount NUMERIC, tolls_amount NUMERIC, improvement_surcharge NUMERIC, total_amount NUMERIC);")
+        with postgresql:
+            st.subheader("PostgreSQL - {0:4.1f} sec".format ((postgresql_end_time - postgresql_start_time))) 
+            st.dataframe(df_pg.set_index(df_pg.columns[0]))
+            st.write(f"**Create a pg table:**")
+            st.write("CREATE TABLE  rides (vendor_id TEXT, pickup_datetime TIMESTAMP WITHOUT TIME ZONE NOT NULL, dropoff_datetime TIMESTAMP WITHOUT TIME ZONE NOT NULL, passenger_count NUMERIC, trip_distance NUMERIC, pickup_longitude NUMERIC, pickup_latitude NUMERIC, rate_code INTEGER, dropoff_longitude NUMERIC, dropoff_latitude  NUMERIC, payment_type INTEGER, fare_amount NUMERIC, extra NUMERIC, mta_tax NUMERIC, tip_amount NUMERIC, tolls_amount NUMERIC, improvement_surcharge NUMERIC, total_amount NUMERIC);")
