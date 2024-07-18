1aa3796f7a7dd0be9d9d93f215f534aba5d27a13
cez-timescale <cez@timescale.com>
2024-07-18 14:25:03 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,21 +10,21 @@ st.write("SQL Executed: SELECT rate_code, COUNT(vendor_id) AS num_trips FROM rid
 conn = st.connection("postgresql", type="sql")
 
 # Perform hypertable query.
-start_time = time.time()
+hypertable_start_time = time.time()
 df = conn.query('SELECT rate_code, COUNT(vendor_id) AS num_trips FROM rides GROUP BY rate_code ORDER BY rate_code;', ttl="0")
-end_time = time.time()
+hypertable_end_time = time.time()
 
 # Print results.
-st.write("Hypertable Query Complete - Elapsed {0:4.1f}s".format ((end_time - start_time)))  
+st.write("Hypertable Query Complete - Elapsed {0:4.1f}s".format ((hypertable_end_time - hypertable_start_time)))  
 st.dataframe(df.set_index(df.columns[0]))
 
 # Perform pg query.
-start_time = time.time()
+postgresql_start_time = time.time()
 df_pg = conn.query('SELECT rate_code, COUNT(vendor_id) AS num_trips FROM rides_pg_table GROUP BY rate_code ORDER BY rate_code;', ttl="0")
-end_time = time.time()
+postgresql_end_time = time.time()
 
 # Print results.
-st.write("PostgreSQL Query Complete - Elapsed {0:4.1f}s".format ((end_time - start_time))) 
+st.write("PostgreSQL Query Complete - Elapsed {0:4.1f}s".format ((postgresql_end_time - postgresql_start_time))) 
 st.dataframe(df_pg.set_index(df_pg.columns[0]))
 
 data_container = st.container()
@@ -32,8 +32,8 @@ data_container = st.container()
 with data_container:
     timescale, postgresql = st.columns(2)
     with timescale:
-        st.write("Hypertable Query Complete - Elapsed {0:4.1f}s".format ((end_time - start_time)))  
+        st.write("Hypertable Query Complete - Elapsed {0:4.1f}s".format ((timescale_end_time - timescale_start_time)))  
         st.dataframe(df.set_index(df.columns[0]))
     with postgresql:
-        st.write("PostgreSQL Query Complete - Elapsed {0:4.1f}s".format ((end_time - start_time))) 
+        st.write("PostgreSQL Query Complete - Elapsed {0:4.1f}s".format ((postgresql_end_time - postgresql_start_time))) 
         st.dataframe(df_pg.set_index(df_pg.columns[0]))
