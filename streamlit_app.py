c0194dcf5c6f43a73dde639efa76bc29964d0807
cez-timescale <cez@timescale.com>
2024-07-18 14:46:54 +0000

    uodate

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,4 +36,4 @@ with data_container:
         st.write("PostgreSQL Query Complete - Elapsed {0:4.1f}s".format ((postgresql_end_time - postgresql_start_time))) 
         st.dataframe(df_pg.set_index(df_pg.columns[0]))
         st.write("/* Create a pg table */")
-        st.write("CREATE TABLE "rides"(vendor_id TEXT, pickup_datetime TIMESTAMP WITHOUT TIME ZONE NOT NULL, ..., total_amount NUMERIC);");
+        st.write("CREATE TABLE rides(vendor_id TEXT, pickup_datetime TIMESTAMP WITHOUT TIME ZONE NOT NULL, ..., total_amount NUMERIC);");
