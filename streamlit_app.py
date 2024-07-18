6802ec6336a9530f4dab15d4d85d9431d677ce62
cez-timescale <cez@timescale.com>
2024-07-18 18:49:24 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title("TimescaleDB vs PostgreSQL")
 st.write("Try Timescale for Free [Link](https://www.timescale.com/?utm_source=google&utm_medium=cpc&utm_campaign=brand&utm_term=timescale&utm_campaign=g_search_brand&utm_source=adwords&utm_medium=ppc&hsa_acc=9771591554&hsa_cam=20915411738&hsa_grp=163076503891&hsa_ad=686577754033&hsa_src=g&hsa_tgt=kwd-11120288971&hsa_kw=timescale&hsa_mt=p&hsa_net=adwords&hsa_ver=3&gad_source=1&gclid=Cj0KCQjw-uK0BhC0ARIsANQtgGNVDQ2KKT5_xr-baYDL-aLL-qiLpHnEl23YqAjm-dM3oL3AEB8DMj8aAvIbEALw_wcB).")
 
 # Set up tabs
-hypertable_tab, compression_tab, continuous_aggregation_tab, pg_vector_tab = st.tabs(["Hypertable", "Compression", "Continuous Aggregation", "GenAI"])
+hypertable_tab, compression_tab, continuous_aggregation_tab, pg_vector_tab, data_retention_tab = st.tabs(["Hypertable", "Compression", "Continuous Aggregation", "GenAI", "Data Retention"])
 
 with hypertable_tab:
     st.write("Querying 10M rides table from NYC cab dataset")
@@ -53,4 +53,7 @@ with continuous_aggregation_tab:
     st.write("WIP")
 
 with pg_vector_tab:
+    st.write("WIP")
+
+with data_retention_tab:
     st.write("WIP")
