b5712bcfb7d6fca1925cd6491359484b6c787002
cez-timescale <cez@timescale.com>
2024-07-18 04:13:36 +0000

    update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,10 +16,12 @@ conn = st.connection("postgresql", type="sql")
 df = conn.query('SELECT rate_code, COUNT(vendor_id) AS num_trips FROM rides GROUP BY rate_code ORDER BY rate_code;', ttl="0")
 
 # Print results.
+st.write("Hypertable Query")
 st.dataframe(df, use_container_width=True)
 
 # Perform query.
 df_pg = conn.query('SELECT rate_code, COUNT(vendor_id) AS num_trips FROM rides_pg_table GROUP BY rate_code ORDER BY rate_code;', ttl="0")
 
 # Print results.
+st.write("PG Table Query")
 st.dataframe(df_pg, use_container_width=True)
